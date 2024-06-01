884620c65019b0866deedf58860351957f1ea0ca
bpisak-l <bpisak-l@student.42.fr>
2024-06-01 15:18:24 +0200

    Make it work for 1 pipe

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bpisak-l <bpisak-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:41:46 by bpisak-l          #+#    #+#             */
-/*   Updated: 2024/06/01 12:45:51 by bpisak-l         ###   ########.fr       */
+/*   Updated: 2024/06/01 15:17:58 by bpisak-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,25 @@ void	execute_command(char **argv, char **env)
 	free(cmd);
 }
 
+int	error_code(int ext)
+{
+	if (WIFSIGNALED(ext))
+		return (128 + WTERMSIG(ext));
+	else if (WIFEXITED(ext))
+		return (WEXITSTATUS(ext));
+	else
+		return (ext);
+}
+
 int	main(int argc, char *argv[], char **env)
 {
 	char	**cmd_set;
 	char	***cmds_set;
 	int		i;
+	int		fd[2];
+	int		pid0;
+	int		pid1;
+	int		exit;
 
 	if (argc != 2)
 		return (0);
@@ -58,9 +72,33 @@ int	main(int argc, char *argv[], char **env)
 	}
 	free_split_arr(cmd_set);
 	i = -1;
-	while (cmds_set[++i])
+	if (pipe(fd) < 0)
+		set_error("pipe", 0);
+	pid0 = fork();
+	if (!pid0)
 	{
-		execute_command(cmds_set[i], env);
+		close(fd[0]);
+		if (dup2(fd[1], 1) == -1)
+			set_error("dup2", 0);
+		close(fd[1]);
+		execute_command(cmds_set[0], env);
 	}
+	pid1 = fork();
+	if (!pid1)
+	{
+		close(fd[1]);
+		if (dup2(fd[0], 0) == -1)
+			set_error("dup2", 0);
+		close(fd[0]);
+		execute_command(cmds_set[1], env);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid0, &exit, 0);
+	waitpid(pid1, &exit, 0);
+	// while (cmds_set[++i])
+	// {
+	// 	execute_command(cmds_set[i], env);
+	// }
 	return (0);
 }
