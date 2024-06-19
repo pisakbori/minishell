af5350d0a9e36bfd1ff5f985cc996e7bf40a32d7
bpisak-l <bpisak-l@student.42.fr>
2024-06-19 18:07:49 +0200

    Little refactor

--- syntax_check.c
+++ syntax_check.c
@@ -6,14 +6,13 @@
 /*   By: bpisak-l <bpisak-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 13:25:30 by bpisak-l          #+#    #+#             */
-/*   Updated: 2024/06/19 15:17:18 by bpisak-l         ###   ########.fr       */
+/*   Updated: 2024/06/19 18:08:18 by bpisak-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
 // TODO:check for > empty, or > |
-// for > < >> <<
 int	is_valid_syntax(char *str)
 {
 	while (*str)
@@ -24,8 +23,8 @@ int	is_valid_syntax(char *str)
 			str = ft_strchr(str + 1, '\'');
 		if (!str)
 		{
-			set_exit_code(2);
-			ft_printf(2, "Syntax error: Unterminated quoted string\n");
+			set_error("minishell", 2,
+					"Syntax error: Unterminated quoted string\n");
 			return (0);
 		}
 		str++;
